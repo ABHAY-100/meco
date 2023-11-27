2adee7f984287240b345238580384c53cdd5ab85
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2023-11-27 20:31:51 +0530

    MECO to meco

--- src/components/comingsoon/components/ComingSoonBottomNav.jsx
+++ src/components/comingsoon/components/ComingSoonBottomNav.jsx
@@ -12,10 +12,10 @@ const ComingSoonBottomNav = () => {
     const subject = 'Expression of Interest in Contributing';
     const body = encodeURIComponent(`
     Kindly provide the following details in your response:
-    1. Could you articulate the motivation behind your interest in contributing to the MECO community?
+    1. Could you articulate the motivation behind your interest in contributing to the meco community?
     2. Share highlights of your past experiences that you believe are relevant to this opportunity.
-    3. What specific skills or qualities do you possess that you feel align with the ethos of MECO.app?
-    4. How do you envisage your contribution shaping the overarching narrative of MECO?
+    3. What specific skills or qualities do you possess that you feel align with the ethos of meco.app?
+    4. How do you envisage your contribution shaping the overarching narrative of meco?
     5. Please provide your professional portfolio and resume for our review.
     `);
     window.location.href = `mailto:${emailAddress}?subject=${subject}&body=${body}`;
