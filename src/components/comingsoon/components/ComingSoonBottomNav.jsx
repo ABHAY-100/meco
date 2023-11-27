270b84c16720370959e1fe69a3646d84f914707d
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2023-11-27 20:29:26 +0530

    updated contibute now

--- src/components/comingsoon/components/ComingSoonBottomNav.jsx
+++ src/components/comingsoon/components/ComingSoonBottomNav.jsx
@@ -9,11 +9,14 @@ import '../components/ComingSoonBottomNav.css'
 const ComingSoonBottomNav = () => {
   const handleSupportButtonClick = () => {
     const emailAddress = 'meco.apppp@gmail.com';
-    const subject = 'Support Development';
+    const subject = 'Expression of Interest in Contributing';
     const body = encodeURIComponent(`
-      In what ways do you envision contributing to MECO, and how do your skills and experiences align with the objectives and values of the organization?
-
-      Note: Please delete this text in the body before sending.
+    Kindly provide the following details in your response:
+    1. Could you articulate the motivation behind your interest in contributing to the MECO community?
+    2. Share highlights of your past experiences that you believe are relevant to this opportunity.
+    3. What specific skills or qualities do you possess that you feel align with the ethos of MECO.app?
+    4. How do you envisage your contribution shaping the overarching narrative of MECO?
+    5. Please provide your professional portfolio and resume for our review.
     `);
     window.location.href = `mailto:${emailAddress}?subject=${subject}&body=${body}`;
   };
@@ -21,7 +24,7 @@ const ComingSoonBottomNav = () => {
   return (
     <div className="bottomNav">
       <div className="left" onClick={handleSupportButtonClick}>
-        <p>Support Development</p>
+        <p>Contribute Now</p>
         <FaArrowRight className="arrow" style={{ color: '#ffffff' }} />
       </div>
       
