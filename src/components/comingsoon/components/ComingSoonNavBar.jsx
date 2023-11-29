7306cc4b2a1ec6b4aadec44e0637a489da8159db
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2023-11-29 06:28:15 +0530

    menu updated

--- src/components/comingsoon/components/ComingSoonNavBar.jsx
+++ src/components/comingsoon/components/ComingSoonNavBar.jsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import { FaCircle } from 'react-icons/fa';
+import React from "react";
+import { FaCircle } from "react-icons/fa";
 import { FaBarsStaggered } from "react-icons/fa6";
-import '../components/ComingSoonNavBar.css';
+import "../components/ComingSoonNavBar.css";
 
 const ComingSoonNavBar = () => {
   return (
     <nav>
       <div className="left">
-        <a href="https://meco.vercel.app/"><img src="/meco-icon-blank.png" alt="meco logo" /></a>
+        <a href="https://meco.vercel.app/">
+          <img src="/meco-icon-blank.png" alt="meco logo" />
+        </a>
       </div>
 
-
       <div className="right">
-        <ul className='menu-long'>
-          <li className='navtext'>Contacts</li>
-            <FaCircle className='dot' style={{ color: '#ffffff' }} />
-          <li className='navtext'>About</li>
+        <ul className="menu-long">
+          <li className="navtext">Contacts</li>
+          <FaCircle className="dot" style={{ color: "#ffffff" }} />
+          <li className="navtext">About</li>
         </ul>
+
+        {/* <div className="menu">
+          <div className="menu_button">
+            <FaBarsStaggered className='menubar' style={{ color: '#ffffff' }} />
+          </div>
+        </div> */}
 
         <div className="menu">
           <div className="menu_button">
-            <FaBarsStaggered className='menubar' style={{ color: '#ffffff' }} />
+          <label for="check">
+            <input type="checkbox" id="check" />
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
           </div>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default ComingSoonNavBar;
