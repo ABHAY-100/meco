b748072c6d3dfa87f13d90608b0a9abee0d5fdf9
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2023-12-01 17:37:36 +0530

    bg

--- src/components/comingsoon/components/ComingSoonBG.jsx
+++ src/components/comingsoon/components/ComingSoonBG.jsx
@@ -5,11 +5,18 @@ const ComingSoonBG = () => {
   return (
     <div>
       <div class="container">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className="Ellipse1" />
+        <div className="Ellipse2" />
+        <div className="Ellipse3" />
+        <div className="Ellipse4" />
+        <div className="Ellipse5" />
+        <div className="Ellipse6" />
+        <div className="Ellipse7" />
+        <div className="Ellipse8" />
+        <div className="Ellipse9" />
+        <div className="Ellipse10" />
+        {/* <div className="Ellipse11" />
+        <div className="Ellipse12" /> */}
       </div>
       <img src="/noise.png" className="noise" />
     </div>
