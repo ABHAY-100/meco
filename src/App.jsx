dcd6d78af63bdc0353a757a4ca6a6fac29b8658b
ABHAY-100 <abhaybalakrishnan884@gmail.com>
2023-12-02 14:33:30 +0530

    loading files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import ComingSoon from './pages/comingsoon/ComingSoon'
+import Loading from './pages/loading/Loading'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <ComingSoon />
+      <Loading />
     </>
   )
 }
---
+++ src/pages/loading/Loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="loading-container"></div>
+  )
+}
+
+export default Loading
