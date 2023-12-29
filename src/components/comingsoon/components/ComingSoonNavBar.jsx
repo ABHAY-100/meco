ca1e37e7e22571217d042e78c86560a60985fb14
ABHAY <abhaybalakrishnan884@gmail.com>
2023-12-29 22:11:53 +0530

    test

--- src/components/comingsoon/components/ComingSoonNavBar.jsx
+++ src/components/comingsoon/components/ComingSoonNavBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FaCircle } from "react-icons/fa";
-import { FaBarsStaggered } from "react-icons/fa6";
+import { FaBarsStaggered } from "react-icons/fa6"; {/* edit required*/}
 import "../components/ComingSoonNavBar.css";
 
 const ComingSoonNavBar = () => {
@@ -8,7 +8,7 @@ const ComingSoonNavBar = () => {
     <nav>
       <div className="left">
         <a href="https://meco.vercel.app/">
-          <img src="/meco-icon-blank.png" alt="meco logo" />
+          <img src="/meco-icon-blank.png" alt="meco logo" /> {/* icon edit required*/}
         </a>
       </div>
 
