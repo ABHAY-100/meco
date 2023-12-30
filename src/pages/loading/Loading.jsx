75e6bacd67790f12264f0818ed1b6bb930bd1ea3
ABHAY <abhaybalakrishnan884@gmail.com>
2023-12-30 06:59:52 +0530

    fix: deployment error fixed

--- src/pages/loading/Loading.jsx
+++ src/pages/loading/Loading.jsx
@@ -1,1 +1,11 @@
-// In Progress: aadi is working on this file.
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div>
+      {/* In Progress: aadi is working on this file. */}
+    </div>
+  )
+}
+
+export default Loading
