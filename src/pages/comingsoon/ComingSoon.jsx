56a23f2bec0338f7ff8192d4c4d99056492fe5ce
ABHAY <abhaybalakrishnan884@gmail.com>
2023-12-31 13:53:47 +0530

    some updates

--- src/pages/comingsoon/ComingSoon.jsx
+++ src/pages/comingsoon/ComingSoon.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ComingSoonContents from '../../components/comingsoon/ComingSoonContents'
-import '../comingsoon/ComingSoon.css'
 
 const ComingSoon = () => {
   return (
